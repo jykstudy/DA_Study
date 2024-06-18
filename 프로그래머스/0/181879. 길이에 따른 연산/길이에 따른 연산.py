40a4beb0b5cf549261f30963bf2549a6e6112dfc
jykstudy <98120cho@naver.com>
2024-06-18 17:44:14 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    for i in num_list:
+        if len(num_list)>=11:
+            return sum(num_list)
+        else:
+            result=1
+            for i in num_list:
+                result*=i
+            return result
