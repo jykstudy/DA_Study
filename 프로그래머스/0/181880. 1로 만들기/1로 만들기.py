0e0e6ff14f00e23c026397a528dc691b0e0323e7
jykstudy <98120cho@naver.com>
2024-07-08 17:40:05 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    total_operations = 0
+    
+    for num in num_list:
+        while num != 1:
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num = (num - 1) // 2
+            total_operations += 1
+    
+    return total_operations
